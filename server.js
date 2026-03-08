b3b30447aaf74055a6912ce7715111f206931480
kesshav28 <kv2159916@gmail.com>
2026-03-08 15:41:42 +0000

    Content Updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps 2-Tier CI/CD Working 🚀");
+  res.send("DevOps 3-Tier CI/CD Application Working 🚀");
 });
 
 app.listen(3000, () => {
